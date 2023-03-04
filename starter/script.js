c3c3e41fcf4194ad8bf8bc3d941d40e48e9fe598
Elias Hakimi <Elias_Hakimi00@outlook.com>
2023-03-04 18:54:43 +0000

    timeblocks added

--- starter/script.js
+++ starter/script.js
@@ -16,3 +16,37 @@ const displayToday = () => {
   $('#currentDay').text(today);
 };
 
+const fillTimeTable = () => {
+  for (var hour = 0; hour < totalHours; hour++) { 
+    var realHour = hour + 9;
+    // add one hour while iterating for loop
+    timeTableElement = hourBeginning.add(1,'h').format('HH:mm A');
+
+    // determine the currentState based on the conditions
+    if (currentHour == realHour) {
+        currentState = 'present';
+    } else if (currentHour < realHour) {
+        currentState = 'past';
+    } else {
+        currentState = 'future';
+    }
+
+    var appendBlock = 
+        `<div id="hour-${realHour}" class="row time-block ${currentState}">
+            <div class="col-md-1 hour">${timeTableElement}</div>
+            <textarea class="col-md-10 description ${realHour}"></textarea>
+            <button class="btn saveBtn col-md-1">
+                <i class="fas fa-save"></i>
+            </button>
+        </div>`;
+
+    $(".container").append(appendBlock);
+    }
+  loadSchedule();
+}
+
+// Function calls
+displayToday();
+fillTimeTable();
+$('.saveBtn').on('click', saveSchedule);
+
